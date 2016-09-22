5612dbbc7bf112a7f5fe17e6599ff83b50f4b363
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-09-22 14:19:48 +0900

    Execute scaffold task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160922044836) do
+ActiveRecord::Schema.define(version: 20160922050549) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "text"
+    t.date     "date"
+    t.integer  "status"
+    t.integer  "priority"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
