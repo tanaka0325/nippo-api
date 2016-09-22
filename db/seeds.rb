d816214b6e9533ba6e159db3a6c2b7bee6e84b37
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-09-22 14:31:07 +0900

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: 'Hiroyuki Tanaka', email: 'h.tanaka.0325@gmail.com')
+User.create(name: "John Smith", email: 'test@te.st')
+
+Task.create(user_id: User.find_by_name("Hiroyuki Tanaka").id, text: 'Buy the milk', date: Date.today, status: 1, priority: nil)
+Task.create(user_id: User.find_by_name("Hiroyuki Tanaka").id, text: 'Study Rails5', date: Date.today, status: 2, priority: 1)
+Task.create(user_id: User.find_by_name("John Smith").id, text: 'Introduce myself', date: Date.today, status: 1, priority: 2)
