4ca259302f21efe6cbe0542151c45452130ca375
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-09-22 15:47:03 +0900

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'date#index'
+
+  resources :tags
+  resources :diaries
   resources :tasks
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
