46d5a56963a13a3fc8a241ac33e7d89c0f7f2dd9
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-09-23 17:49:21 +0900

    Remove date controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'date#index'
   resources :tags
   resources :diaries
   resources :tasks
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  match 'date', :controller => 'date', :action => :index, :via => :get
-  match 'date/:date', :controller => 'date', :action => :show, :via => :get
 end
