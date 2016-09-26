c3326b70f37bedc2e475a6a24c89b025a85d41ee
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-09-26 20:39:32 +0900

    Add limit 5

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -6,7 +6,7 @@ class TimelineController < ApplicationController
 
   def date
     @date = Date.strptime(params[:date], "%Y-%m-%d")
-    @logs = ActionLog.where(created_at: @date.beginning_of_day..@date.end_of_day).order(created_at: :desc)
+    @logs = ActionLog.where(created_at: @date.beginning_of_day..@date.end_of_day).order(created_at: :desc).limit(5)
     render json: @logs
   end
 end
