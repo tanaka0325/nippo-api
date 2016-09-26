ee77e7255b70641afff555e67a74017f5b629837
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-09-26 18:07:20 +0900

    Change sort to created_at:desc

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -6,8 +6,7 @@ class TimelineController < ApplicationController
 
   def date
     @date = Date.strptime(params[:date], "%Y-%m-%d")
-    @logs = ActionLog.where(created_at: @date.beginning_of_day..@date.end_of_day)
-    p @logs.first.action.action_class
+    @logs = ActionLog.where(created_at: @date.beginning_of_day..@date.end_of_day).order(created_at: :desc)
     render json: @logs
   end
 end
