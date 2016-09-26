62827383e5332f4b58cc842a6a37cc53e8b5d619
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-09-26 18:07:34 +0900

    Implement create action log when post tweet and add task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,17 @@
 class Task < ApplicationRecord
   belongs_to :user
+
+  after_save :add_action_log
+
+  def add_action_log
+    @task_log = ActionLog.new({action_id: 4, target_id: self.id})
+
+    if @task_log.save
+      return true
+    else
+      raise ActiveRecord::Rollback
+      return false
+    end
+    self
+  end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,18 @@
 class Tweet < ApplicationRecord
   belongs_to :user
+
+  after_save :add_action_log
+
+  def add_action_log
+    @tweet_log = ActionLog.new({action_id: 1, target_id: self.id})
+
+    if @tweet_log.save
+      return true
+    else
+      raise ActiveRecord::Rollback
+      return false
+    end
+    self
+  end
+
 end
