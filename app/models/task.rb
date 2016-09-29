df79b2fd1e39b8fb9f2547d6892d9d1f0291c1ee
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-09-29 21:06:02 +0900

    Destroy action_log when destory relation task

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,7 @@ class Task < ApplicationRecord
   belongs_to :user
 
   after_save :add_action_log
+  after_destroy :destroy_action_log
 
   def add_action_log
     @task_log = ActionLog.new({action_id: 4, target_id: self.id, action_class_id: 2})
@@ -14,4 +15,17 @@ class Task < ApplicationRecord
     end
     self
   end
+
+  def destroy_action_log
+    @task_log = ActionLog.find_by(self.id);
+
+    if @task_log.destory
+      return true
+    else
+      raise ActiveRecord::Rollback
+      return false
+    end
+    self
+  end
+
 end
