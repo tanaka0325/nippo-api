c4210836c0b494092cd5ecf876dca56308ec2ef9
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-09-30 21:37:51 +0900

    Sort by created_at in response json

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -2,12 +2,12 @@ class TimelineController < ApplicationController
   def index
     @task_actions = TaskAction.all
     @tweet_actions = TweetAction.all
-    render json: [@task_actions, @tweet_actions].flatten
+    render json: [@task_actions, @tweet_actions].flatten.sort_by{|a| a.created_at}.reverse
   end
 
   def date
     @task_actions = TaskAction.where(date: params[:date])
     @tweet_actions = TweetAction.where(date: params[:date])
-    render json: [@task_actions, @tweet_actions].flatten
+    render json: [@task_actions, @tweet_actions].flatten.sort_by{|a| a.created_at}.reverse
   end
 end
